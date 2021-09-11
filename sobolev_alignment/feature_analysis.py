ae23a93d8bc979b90cdf053d7d9e9498d485a04e
saroudant <soufiane.mourragui@free.fr>
2021-09-11 12:07:08 +0200

    error when no covariates

--- sobolev_alignment/feature_analysis.py
+++ sobolev_alignment/feature_analysis.py
@@ -16,6 +16,7 @@ References
 
 import numpy as np
 import pandas as pd
+import gc
 import scipy
 from itertools import combinations_with_replacement
 from joblib import Parallel, delayed
@@ -36,15 +37,6 @@ def basis(x, k, gamma):
 
     return coef * product
 
-# def basis(x, k, gamma):
-#     if k == 0:
-#         return np.ones(x.shape[0])
-#     product = x
-#     for i in range(1,k):
-#         product = np.multiply(x,product)
-#     coef = np.power(2*gamma,k/2) / np.sqrt(scipy.math.factorial(k))
-
-#     return coef * product
 
 def combinatorial_product(x, idx, gamma):
     prod = [
@@ -53,23 +45,7 @@ def combinatorial_product(x, idx, gamma):
     ]
     if len(prod) == 0:
         return 1
-    return reduce(scipy.sparse.csr_matrix.multiply, prod)
-
-# def combinatorial_product(x, idx, gamma):
-#     prod = np.prod([
-#         basis(x[:,i], k, gamma)
-#         for i,k in enumerate(combination_to_idx(idx, x.shape[1]))
-#     ], axis=0)
-#     return scipy.sparse.csr_matrix(prod)
-
-
-# def interaction_name(gene_names, combi):
-#     involved_genes = np.where(combi > 0)
-#     combin_name = [
-#         '%s^%s' % (gene_names[idx], combi[idx])
-#         for idx in involved_genes
-#     ]
-#     return '*'.join(combin_name) if len(combin_name) > 0 else '1'
+    return reduce(scipy.sparse.csc_matrix.multiply, prod)
 
 def interaction_name(gene_combi):
     combin_name = [
@@ -84,6 +60,7 @@ def higher_order_interaction_wrapper(data, x, gamma, gene_names):
         interaction_name(gene_names, combination_to_idx(x, data.shape[1]))
     ]
 
+
 def higher_order_contribution(
         d: int,
         data: np.array,
@@ -92,83 +69,38 @@ def higher_order_contribution(
         gamma: float,
         n_jobs=1
 ):
-    sparse_data = scipy.sparse.csr_matrix(data)
+    sparse_data = scipy.sparse.csc_matrix(data)
     print('\t START FEATURES', flush=True)
-    combinations_features = Parallel(n_jobs=n_jobs, verbose=1, max_nbytes=1e6)(
+    combinations_features = Parallel(n_jobs=n_jobs, verbose=1, max_nbytes=1e6, pre_dispatch=int(1.5*n_jobs))(
         delayed(combinatorial_product)(sparse_data, x, gamma)
         for x in combinations_with_replacement(np.arange(sparse_data.shape[1]), r=d)
     )
+    gc.collect()
 
     print('\t START CONCATENATION', flush=True)
-    combinations_features = scipy.sparse.hstack(combinations_features)
-    combinations_features = scipy.sparse.diags(sample_offset).dot(combinations_features).todense()
+    print('\t\t START STACKING', flush=True)
+    combinations_features = scipy.sparse.hstack(combinations_features, format='csc')
+    print('\t\t START PRODUCT', flush=True)
+    combinations_features = scipy.sparse.diags(sample_offset).dot(combinations_features)
+    print('\t\t DENSIFY', flush=True)
+    gc.collect()
+    # combinations_features = combinations_features.todense()
 
-    combinations_names = Parallel(n_jobs=min(5,n_jobs), verbose=1, max_nbytes=1e4)(
+    print('\t\t FIND NAMES', flush=True)
+    combinations_names = Parallel(n_jobs=min(5,n_jobs), verbose=1, max_nbytes=1e4, pre_dispatch=int(1.5*min(5,n_jobs)))(
         delayed(interaction_name)(x)
         for x in combinations_with_replacement(gene_names, r=d)
     )
 
-    return pd.DataFrame(
+    # return pd.DataFrame(
+    #     data=combinations_features,
+    #     columns=combinations_names
+    # )
+    return pd.DataFrame.sparse.from_spmatrix(
         data=combinations_features,
         columns=combinations_names
     )
-
-# def higher_order_contribution(
-#         d: int,
-#         data: np.array,
-#         sample_offset: np.array,
-#         gene_names: list,
-#         gamma: float,
-#         n_jobs=1
-# ):
-#     # sparse_data = scipy.sparse.csr_matrix(data)
-#     print('\t START FEATURES', flush=True)
-#     combinations_features = Parallel(n_jobs=n_jobs, verbose=1, max_nbytes=1e6)(
-#         delayed(combinatorial_product)(data, x, gamma)
-#         for x in combinations_with_replacement(np.arange(data.shape[1]), r=d)
-#     )
     
-#     print('\t START CONCATENATION', flush=True)
-#     combinations_features = scipy.sparse.vstack(combinations_features).T
-#     combinations_features = scipy.sparse.diags(sample_offset).dot(combinations_features).todense()
-
-#     combinations_names = [
-#         interaction_name(gene_names, combination_to_idx(x, data.shape[1]))
-#         for x in combinations_with_replacement(np.arange(data.shape[1]), r=d)
-#     ]
-#     print(len(combinations_names))
-
-#     return pd.DataFrame(
-#         data=combinations_features,
-#         columns=combinations_names
-#     )
-
-# def higher_order_contribution(
-#         d: int,
-#         data: np.array,
-#         sample_offset: np.array,
-#         gene_names: list,
-#         gamma: float,
-#         n_jobs=1
-# ):
-#     print('START %s' % (d), flush=True)
-#     combinations = []
-#     combination_names = []
-#     for x in combinations_with_replacement(np.arange(data.shape[1]), r=d):
-#         combinations.append(x)
-#         # combinations.append(combinatorial_product(data, x, gamma))
-#         combination_names.append(interaction_name(gene_names, combination_to_idx(x, data.shape[1])))
-#     combinations = Parallel(n_jobs=n_jobs, verbose=1)(
-#         delayed(combinatorial_product)(data, x, gamma) for x in combinations
-#     )
-#
-#     combinations_features = np.array(combinations).T
-#     combinations_features = np.diag(sample_offset).dot(combinations_features)
-#
-#     return pd.DataFrame(
-#         data=combinations_features,
-#         columns=combination_names
-#     )
 
 def _compute_offset(data, gamma):
     sample_offset = np.linalg.norm(data, axis=1)
--- sobolev_alignment/generate_artificial_sample.py
+++ sobolev_alignment/generate_artificial_sample.py
@@ -106,7 +106,14 @@ def parallel_generate_samples(sample_size,
                               batch_size=10**3,
                               n_jobs=1):
     results = Parallel(n_jobs=n_jobs, verbose=1)(
-            delayed(generate_samples)(batch_size, batch_names[i:i+batch_size], covariates_values[i:i+batch_size], lib_size, model, return_dist)
+            delayed(generate_samples)(
+                batch_size, 
+                batch_names[i:i+batch_size] if batch_names is not None else None, 
+                covariates_values[i:i+batch_size] if covariates_values is not None else None, 
+                lib_size, 
+                model, 
+                return_dist
+            )
             for i in range(0,sample_size,batch_size)
     )
 
